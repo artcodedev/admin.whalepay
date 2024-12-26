1cdc08fe4f135d643dec34376241f088681aacd5
art[code] <you@example.com>
2024-12-26 19:43:04 +1000

    3

--- app/Components/WithdrawDialog.tsx
+++ app/Components/WithdrawDialog.tsx
@@ -11,6 +11,7 @@ import Select, { SelectChangeEvent } from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import DialogActions from "@mui/material/DialogActions";
 
 
 interface Props {
@@ -32,7 +33,7 @@ const WithdrawDialog = ({ ...pr }: Props) => {
 
     return (
         <>
-            <Dialog open={pr.open} onClose={pr.onClose} scroll="paper" >
+            <Dialog open={pr.open} onClose={pr.onClose} scroll="paper" fullWidth={true} >
 
                 <DialogTitle id="scroll-dialog-title">Создать вывод средств</DialogTitle>
 
@@ -57,22 +58,21 @@ const WithdrawDialog = ({ ...pr }: Props) => {
                                 </Box>
 
                                 <Box sx={{ marginTop: '10px' }}>
-                                    <TextField id="outlined-basic" label="Сумма" variant="outlined" />
+                                    <TextField id="outlined-basic" label="Сумма" variant="outlined" sx={{ width: '100%' }} />
                                 </Box>
 
-                                <Box sx={{ marginTop: '10px' }}>
+                                <DialogActions sx={{ padding: '0px', marginTop: '10px' }}>
+
                                     <Button variant="contained" color='success' sx={{ width: '100%' }}>Выполнить</Button>
-                                </Box>
-
-                                <Box sx={{ marginTop: '10px' }}>
                                     <Button variant="contained" color="error" sx={{ width: '100%' }}>Отмена</Button>
-                                </Box>
 
+                                </DialogActions>
 
                             </Box>}
                     </DialogContentText>
 
                 </DialogContent>
+
             </Dialog>
 
 
